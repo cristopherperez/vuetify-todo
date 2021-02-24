49a4618961875e2831cbce5ad864d75854287922
cristopherperez <cristopherperez88@gmail.com>
2021-02-24 14:54:56 -0300

    todo app con vuetify

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About vuetify todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro rerum voluptatibus maiores id iste, possimus placeat ipsum fugit. Iste dolore sequi, dolorum totam eum ut debitis! Eum, sint debitis?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae a eaque accusamus iusto dolorem at debitis, alias corrupti accusantium! Ipsum sit corporis quo. Aspernatur quod eveniet error mollitia, nesciunt amet?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A porro rerum voluptatibus maiores id iste, possimus placeat ipsum fugit. Iste dolore sequi, dolorum totam eum ut debitis! Eum, sint debitis?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae a eaque accusamus iusto dolorem at debitis, alias corrupti accusantium! Ipsum sit corporis quo. Aspernatur quod eveniet error mollitia, nesciunt amet?</p>
   </div>
 </template>
